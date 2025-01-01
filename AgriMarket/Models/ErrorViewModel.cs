dcffd8db6eef2e81a905c434a0233a544e73aff4
User <User@DESKTOP-9IR4CHQ>
2025-01-01 14:05:17 +0300

    Add project files.

---
+++ AgriMarket/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgriMarket.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
